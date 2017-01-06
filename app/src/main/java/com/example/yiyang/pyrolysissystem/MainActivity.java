0c237b7d2acfa6a8e8e7263871842b81b31918f4
chiangyiyang <chiangyiyang@gmail.com>
2017-01-06 19:38:51 +0800

    增加Usb Serial Devices列表選單 增加開啟Serial Devices功能

--- app/src/main/java/com/example/yiyang/pyrolysissystem/MainActivity.java
+++ app/src/main/java/com/example/yiyang/pyrolysissystem/MainActivity.java
@@ -1,23 +1,86 @@
 package com.example.yiyang.pyrolysissystem;
 
+import android.content.Context;
 import android.content.Intent;
+import android.hardware.usb.UsbDeviceConnection;
+import android.hardware.usb.UsbManager;
 import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.ShareActionProvider;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CompoundButton;
+import android.widget.ScrollView;
+import android.widget.TextView;
 import android.widget.Toast;
 import android.widget.ToggleButton;
 
+import com.hoho.android.usbserial.driver.UsbSerialPort;
+import com.hoho.android.usbserial.util.HexDump;
+import com.hoho.android.usbserial.util.SerialInputOutputManager;
+
+import java.io.IOException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 public class MainActivity extends AppCompatActivity {
 
+    private final String TAG = MainActivity.class.getSimpleName();
+
+
+    public static final int REQUEST_SERIAL_PORT = 1;
     private boolean isConnect = false;
+
+    private static UsbSerialPort sPort = null;
+
+    private TextView mTitleTextView;
+    private TextView mDumpTextView;
+    private ScrollView mScrollView;
+
+
+    private final ExecutorService mExecutor = Executors.newSingleThreadExecutor();
+
+    private SerialInputOutputManager mSerialIoManager;
+
+    private final SerialInputOutputManager.Listener mListener =
+            new SerialInputOutputManager.Listener() {
+
+                @Override
+                public void onRunError(Exception e) {
+                    Log.d(TAG, "Runner stopped.");
+                }
+
+                @Override
+                public void onNewData(final byte[] data) {
+                    MainActivity.this.runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            MainActivity.this.updateReceivedData(data);
+                        }
+                    });
+                }
+            };
+
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (resultCode == RESULT_OK) {
+            if (requestCode == REQUEST_SERIAL_PORT) {
+                Toast.makeText(this, "Settings Back", Toast.LENGTH_LONG).show();
+
+            }
+        }
+
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +89,10 @@ public class MainActivity extends AppCompatActivity {
 
         Toolbar myToolbar = (Toolbar) findViewById(R.id.main_toolbar);
         setSupportActionBar(myToolbar);
+
+        mTitleTextView = (TextView) findViewById(R.id.demoTitle);
+        mDumpTextView = (TextView) findViewById(R.id.consoleText);
+        mScrollView = (ScrollView) findViewById(R.id.demoScroller);
     }
 
 
@@ -35,7 +102,7 @@ public class MainActivity extends AppCompatActivity {
             case R.id.action_settings:
                 Intent intent = new Intent();
                 intent.setClass(MainActivity.this, SettingsActivity.class);
-                startActivity(intent);
+                startActivityForResult(intent, REQUEST_SERIAL_PORT);
                 return true;
 
             case R.id.action_connect:
@@ -63,4 +130,89 @@ public class MainActivity extends AppCompatActivity {
 
         return true;
     }
+
+
+
+    void showStatus(TextView theTextView, String theLabel, boolean theValue){
+        String msg = theLabel + ": " + (theValue ? "enabled" : "disabled") + "\n";
+        theTextView.append(msg);
+    }
+
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(TAG, "Resumed, port=" + sPort);
+        if (sPort == null) {
+            mTitleTextView.setText("No serial device.");
+        } else {
+            final UsbManager usbManager = (UsbManager) getSystemService(Context.USB_SERVICE);
+
+            UsbDeviceConnection connection = usbManager.openDevice(sPort.getDriver().getDevice());
+            if (connection == null) {
+                mTitleTextView.setText("Opening device failed");
+                return;
+            }
+
+            try {
+                sPort.open(connection);
+                sPort.setParameters(115200, 8, UsbSerialPort.STOPBITS_1, UsbSerialPort.PARITY_NONE);
+
+                showStatus(mDumpTextView, "CD  - Carrier Detect", sPort.getCD());
+                showStatus(mDumpTextView, "CTS - Clear To Send", sPort.getCTS());
+                showStatus(mDumpTextView, "DSR - Data Set Ready", sPort.getDSR());
+                showStatus(mDumpTextView, "DTR - Data Terminal Ready", sPort.getDTR());
+                showStatus(mDumpTextView, "DSR - Data Set Ready", sPort.getDSR());
+                showStatus(mDumpTextView, "RI  - Ring Indicator", sPort.getRI());
+                showStatus(mDumpTextView, "RTS - Request To Send", sPort.getRTS());
+
+            } catch (IOException e) {
+                Log.e(TAG, "Error setting up device: " + e.getMessage(), e);
+                mTitleTextView.setText("Error opening device: " + e.getMessage());
+                try {
+                    sPort.close();
+                } catch (IOException e2) {
+                    // Ignore.
+                }
+                sPort = null;
+                return;
+            }
+            mTitleTextView.setText("Serial device: " + sPort.getClass().getSimpleName());
+        }
+        onDeviceStateChange();
+    }
+
+    private void stopIoManager() {
+        if (mSerialIoManager != null) {
+            Log.i(TAG, "Stopping io manager ..");
+            mSerialIoManager.stop();
+            mSerialIoManager = null;
+        }
+    }
+
+    private void startIoManager() {
+        if (sPort != null) {
+            Log.i(TAG, "Starting io manager ..");
+            mSerialIoManager = new SerialInputOutputManager(sPort, mListener);
+            mExecutor.submit(mSerialIoManager);
+        }
+    }
+
+    private void onDeviceStateChange() {
+        stopIoManager();
+        startIoManager();
+    }
+
+    private void updateReceivedData(byte[] data) {
+        final String message = "Read " + data.length + " bytes: \n"
+                + HexDump.dumpHexString(data) + "\n\n";
+        mDumpTextView.append(message);
+        mScrollView.smoothScrollTo(0, mDumpTextView.getBottom());
+    }
+
+
+    static void setSerialPort(UsbSerialPort port) {
+        sPort = port;
+    }
+
 }
--- app/src/main/java/com/example/yiyang/pyrolysissystem/SettingsActivity.java
+++ app/src/main/java/com/example/yiyang/pyrolysissystem/SettingsActivity.java
@@ -1,15 +1,54 @@
 package com.example.yiyang.pyrolysissystem;
 
-import android.support.v7.app.ActionBar;
+import android.content.Context;
+import android.content.Intent;
+import android.hardware.usb.UsbDevice;
+import android.hardware.usb.UsbManager;
+import android.os.AsyncTask;
+import android.os.Handler;
+import android.os.Message;
+import android.os.SystemClock;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
-import android.widget.Toast;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.TwoLineListItem;
+
+import com.hoho.android.usbserial.driver.UsbSerialDriver;
+import com.hoho.android.usbserial.driver.UsbSerialPort;
+import com.hoho.android.usbserial.driver.UsbSerialProber;
+import com.hoho.android.usbserial.util.HexDump;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class SettingsActivity extends AppCompatActivity {
+
+
+    private final String TAG = SettingsActivity.class.getSimpleName();
+
+    private UsbManager mUsbManager;
+    private ListView mListView;
+    private TextView mProgressBarTitle;
+    private ProgressBar mProgressBar;
+
+    private List<UsbSerialPort> mEntries = new ArrayList<UsbSerialPort>();
+    private ArrayAdapter<UsbSerialPort> mAdapter;
+
+    private static final int MESSAGE_REFRESH = 101;
+    private static final long REFRESH_TIMEOUT_MILLIS = 50000;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +59,65 @@ public class SettingsActivity extends AppCompatActivity {
         Toolbar myToolbar = (Toolbar) findViewById(R.id.settings_toolbar);
         setSupportActionBar(myToolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+
+        mUsbManager = (UsbManager) getSystemService(Context.USB_SERVICE);
+        mListView = (ListView) findViewById(R.id.deviceList);
+        mProgressBar = (ProgressBar) findViewById(R.id.progressBar);
+        mProgressBarTitle = (TextView) findViewById(R.id.progressBarTitle);
+
+        mAdapter = new ArrayAdapter<UsbSerialPort>(this,
+                android.R.layout.simple_expandable_list_item_2, mEntries) {
+            @Override
+            public View getView(int position, View convertView, ViewGroup parent) {
+                final TwoLineListItem row;
+                if (convertView == null) {
+                    final LayoutInflater inflater =
+                            (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+                    row = (TwoLineListItem) inflater.inflate(android.R.layout.simple_list_item_2, null);
+                } else {
+                    row = (TwoLineListItem) convertView;
+                }
+
+                final UsbSerialPort port = mEntries.get(position);
+                final UsbSerialDriver driver = port.getDriver();
+                final UsbDevice device = driver.getDevice();
+
+                final String title = String.format("Vendor %s Product %s",
+                        HexDump.toHexString((short) device.getVendorId()),
+                        HexDump.toHexString((short) device.getProductId()));
+                row.getText1().setText(title);
+
+                final String subtitle = driver.getClass().getSimpleName();
+                row.getText2().setText(subtitle);
+
+                return row;
+            }
+
+        };
+        mListView.setAdapter(mAdapter);
+
+        mListView.setOnItemClickListener(new ListView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Log.d(TAG, "Pressed item " + position);
+                if (position >= mEntries.size()) {
+                    Log.w(TAG, "Illegal position.");
+                    return;
+                }
+
+                final UsbSerialPort port = mEntries.get(position);
+//                showMainActivity(port);
+//                finishActivity(MainActivity.REQUEST_SERIAL_PORT);
+
+                MainActivity.setSerialPort(port);
+
+                Intent intent = new Intent(SettingsActivity.this, MainActivity.class);
+                setResult(RESULT_OK, intent);
+                finish();
+            }
+        });
+
     }
 
 
@@ -27,7 +125,8 @@ public class SettingsActivity extends AppCompatActivity {
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
             case R.id.action_refresh:
-                Toast.makeText(this, "Refresh was selected!", Toast.LENGTH_LONG).show();
+//                Toast.makeText(this, "Refresh was selected!", Toast.LENGTH_LONG).setSerialPort();
+                refreshDeviceList();
                 return true;
 
 
@@ -43,4 +142,84 @@ public class SettingsActivity extends AppCompatActivity {
 
         return true;
     }
+
+
+    private final Handler mHandler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            switch (msg.what) {
+                case MESSAGE_REFRESH:
+                    refreshDeviceList();
+                    mHandler.sendEmptyMessageDelayed(MESSAGE_REFRESH, REFRESH_TIMEOUT_MILLIS);
+                    break;
+                default:
+                    super.handleMessage(msg);
+                    break;
+            }
+        }
+
+    };
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mHandler.sendEmptyMessage(MESSAGE_REFRESH);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mHandler.removeMessages(MESSAGE_REFRESH);
+    }
+
+    private void refreshDeviceList() {
+        showProgressBar();
+
+        new AsyncTask<Void, Void, List<UsbSerialPort>>() {
+            @Override
+            protected List<UsbSerialPort> doInBackground(Void... params) {
+                Log.d(TAG, "Refreshing device list ...");
+                SystemClock.sleep(1000);
+
+                final List<UsbSerialDriver> drivers =
+                        UsbSerialProber.getDefaultProber().findAllDrivers(mUsbManager);
+
+                final List<UsbSerialPort> result = new ArrayList<UsbSerialPort>();
+                for (final UsbSerialDriver driver : drivers) {
+                    final List<UsbSerialPort> ports = driver.getPorts();
+                    Log.d(TAG, String.format("+ %s: %s port%s",
+                            driver, Integer.valueOf(ports.size()), ports.size() == 1 ? "" : "s"));
+                    result.addAll(ports);
+                }
+
+                return result;
+            }
+
+            @Override
+            protected void onPostExecute(List<UsbSerialPort> result) {
+                mEntries.clear();
+                mEntries.addAll(result);
+                mAdapter.notifyDataSetChanged();
+                mProgressBarTitle.setText(
+                        String.format("%s device(s) found", Integer.valueOf(mEntries.size())));
+                hideProgressBar();
+                Log.d(TAG, "Done refreshing, " + mEntries.size() + " entries found.");
+            }
+
+        }.execute((Void) null);
+    }
+
+    private void showProgressBar() {
+        mProgressBar.setVisibility(View.VISIBLE);
+        mProgressBarTitle.setText(R.string.refreshing);
+    }
+
+    private void hideProgressBar() {
+        mProgressBar.setVisibility(View.INVISIBLE);
+    }
+
+//    private void showMainActivity(UsbSerialPort port) {
+//        MainActivity.setSerialPort(port);
+//    }
+
 }
