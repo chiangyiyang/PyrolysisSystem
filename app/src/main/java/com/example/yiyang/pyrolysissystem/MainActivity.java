b7d9e3f9f569c5f0010956b8237d3991fd6ad97b
chiangyiyang <chiangyiyang@gmail.com>
2017-01-06 16:36:22 +0800

    新增SettingsActivity 新增MainActivity及SettingsActivity的Toolbar

--- app/src/main/java/com/example/yiyang/pyrolysissystem/MainActivity.java
+++ app/src/main/java/com/example/yiyang/pyrolysissystem/MainActivity.java
@@ -1,13 +1,68 @@
 package com.example.yiyang.pyrolysissystem;
 
+import android.content.Intent;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.ShareActionProvider;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.Toast;
+import android.widget.ToggleButton;
 
 public class MainActivity extends AppCompatActivity {
+
+    private boolean isConnect = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.main_toolbar);
+        setSupportActionBar(myToolbar);
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_settings:
+//                Toast.makeText(this, "Settings was selected!",Toast.LENGTH_LONG).show();
+                Intent intent = new Intent();
+                intent.setClass(MainActivity.this, SettingsActivity.class);
+                startActivity(intent);
+                return true;
+
+            case R.id.action_connect:
+//                Toast.makeText(this, "Connection was selected!",Toast.LENGTH_LONG).show();
+                if (isConnect) {
+                    //close serial port
+                    item.setIcon(getDrawable(R.drawable.ic_action_play));
+                } else {
+                    //open serial port
+                    item.setIcon(getDrawable(R.drawable.ic_action_stop));
+
+                }
+                isConnect = !isConnect;
+
+                return true;
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(final Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_menu, menu);
+
+        return true;
     }
 }
---
+++ app/src/main/java/com/example/yiyang/pyrolysissystem/SettingsActivity.java
@@ -0,0 +1,46 @@
+package com.example.yiyang.pyrolysissystem;
+
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+public class SettingsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+
+
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.settings_toolbar);
+        setSupportActionBar(myToolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.action_refresh:
+                Toast.makeText(this, "Refresh was selected!", Toast.LENGTH_LONG).show();
+                return true;
+
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(final Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.settings_menu, menu);
+
+        return true;
+    }
+}
